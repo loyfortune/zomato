6783e3c1c88881705159ba0601fe451bd52eadac
loyfortune <loyfortuneofforkansi@gmail.com>
2026-04-02 02:51:26 +0100

    initial commit

---
+++ restaurant-landing-page-assets/app.constants.js
@@ -0,0 +1,83 @@
+export const VIDEO_URL =
+  "https://b.zmtcdn.com/data/file_assets/2627bbed9d6c068e50d2aadcca11ddbb1743095925.mp4";
+
+export const PLAY_STORE_URL =
+  "https://b.zmtcdn.com/data/o2_assets/aad864bd17860b27634fe621001c32db1739350431.png";
+
+export const APP_STORE_URL =
+  "https://b.zmtcdn.com/data/o2_assets/df6464de32f4a09262cee301f65aaa661739351256.png";
+
+export const LEFT_SIDE_LINE =
+  "https://b.zmtcdn.com/data/o2_assets/901001826baf04838b1bf505176ff0b11742453501.png";
+
+export const RIGHT_SIDE_LINE =
+  "https://b.zmtcdn.com/data/o2_assets/901001826baf04838b1bf505176ff0b11742453501.png";
+
+export const BURGER_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/110a09a9d81f0e5305041c1b507d0f391743058910.png";
+
+export const MOMOS_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/b4f62434088b0ddfa9b370991f58ca601743060218.png";
+
+export const PIZZA_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/316495f4ba2a9c9d9aa97fed9fe61cf71743059024.png";
+
+export const TOMATO_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/9ef1cc6ecf1d92798507ffad71e9492d1742892584.png";
+
+export const LEAVES_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/70b50e1a48a82437bfa2bed925b862701742892555.png";
+
+export const RESTAURANT_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/d19ec60986487a77bcb026e5efc3325f1742908200.png";
+
+export const CITIES_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/e7533c4081d6140da37b9f430cb7b8051743006192.png";
+
+export const ORDER_DELIVERED_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/713443cc5944ce4284d7e49e75e2aacf1742466222.png";
+
+export const PHONE_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/3f7e2757e62fd22592b879bd56b666011742294630.png";
+
+export const CALENDAR_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/cc1caf220c91be38dd94cce12b416fcd1746550226.png";
+
+export const HEALTHY_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/d0f1639403f80f8f2c19e0d538222e661742455804.png";
+
+export const VEG_MODE_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/82f145180cd6f920a8a8617dda366a0a1742455963.png";
+
+export const PARTY_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/5e7aab0f183b36fc12c29279f0cb55181742462245.png";
+
+export const GIFT_CARDS_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/867f86a10503998e437963bb37c451591742455764.png";
+
+export const GOURMET_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/6e27c9acde6045c272a28e6eb275727e1742455789.png";
+
+export const OFFERS_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/813952c961fd13588cb71867d84ea7dc1742455815.png";
+
+export const FOOD_ON_TRAIN_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/06d090307e02772693ac06123b53459b1742455939.png";
+
+export const COLLECTIONS_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/5e973dd10c387878009c66d625ae541a1746550690.png";
+
+export const GOLD_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/5f94743e20e3f668953fda8b0e56f2f71742996005.png";
+
+export const FREE_DELIVERY_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/bc9d2a579285cbdaa101b8fe2ba68f601741779645.png";
+
+export const SCOTTER_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/d668ed26c7d4771318d0aa03b3f905e71741779899.png";
+
+export const GOLD_STAR_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/e7a502b732de9cd0d5ceca82306137ca1743059837.png";
+
+export const QR_CODE_IMG_URL =
+  "https://b.zmtcdn.com/data/o2_assets/98cc4eba0a6f59e728e5223a70fd39551742471514.png";
